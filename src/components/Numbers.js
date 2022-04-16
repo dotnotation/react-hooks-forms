da4354f81b4750454d5a0a5418031e66da03167d
dotnotation <dotinmotionproductions@gmail.com>
2022-04-16 15:32:04 -0400

    completed code along

---
+++ src/components/Numbers.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function Numbers() {
+    const [number, setNumber] = useState(0);
+    const [error, setError] = useState(null);
+
+    function handleNumberChange(event) {
+      const newNumber = parseInt(event.target.value);
+      if (newNumber >= 0 && newNumber <= 5) {
+        setNumber(newNumber);
+        setError(null);
+      } else {
+        setError(`${newNumber} is not a valid number!`);
+      }
+    }
+  
+    return (
+      <form>
+        <input type="number" value={number} onChange={handleNumberChange} />
+        {error ? <span style={{ color: "red" }}>{error}</span> : null}
+      </form>
+    );
+}
